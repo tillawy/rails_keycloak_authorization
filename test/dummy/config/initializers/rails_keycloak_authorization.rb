acb07d908bca45437d3aee8fea17ccc323b8fab2
Mohammed O. Tillawy <tillawy@gmail.com>
2024-07-03 08:58:41 +0300

    alignment

--- test/dummy/config/initializers/rails_keycloak_authorization.rb
+++ test/dummy/config/initializers/rails_keycloak_authorization.rb
@@ -3,7 +3,7 @@
 RailsKeycloakAuthorization.client_id = ENV.fetch("KEYCLOAK_AUTH_CLIENT_ID", "dummy-client")
 RailsKeycloakAuthorization.keycloak_server_url = ENV.fetch("KEYCLOAK_SERVER_URL", "http://localhost:8080")
 RailsKeycloakAuthorization.keycloak_realm = ENV.fetch("KEYCLOAK_AUTH_CLIENT_REALM_NAME", "dummy")
-  RailsKeycloakAuthorization.match_patterns = [
+RailsKeycloakAuthorization.match_patterns = [
   /^\/organizations(\.json)?/,
   /^\/api/
 ]
