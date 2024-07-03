e9dca83e124c4b37ff2e40f28b735442283f4df2
Mohammed O. Tillawy <tillawy@gmail.com>
2024-07-03 08:55:17 +0300

    force layout: htmx

--- app/controllers/rails_keycloak_authorization/management_controller.rb
+++ app/controllers/rails_keycloak_authorization/management_controller.rb
@@ -1,6 +1,10 @@
 module RailsKeycloakAuthorization
   class ManagementController < ApplicationController
+    layout false
+
     def index
+      render layout: "rails_keycloak_authorization/htmx"
     end
+
   end
 end
