cd39119c27551ee5338014c8772624fe7bd0e82d
Mohammed O. Tillawy <tillawy@gmail.com>
2024-07-05 11:55:41 +0300

    create route for keycloak-resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 RailsKeycloakAuthorization::Engine.routes.draw do
   root "management#index"
   resources :routes, only: [:index, :show]
-  resources :resources, only: [:create, :show, :new]
+  resources :resources, only: [:create, :show, :new, :index]
   resources :scopes, only: [:index]
 end
